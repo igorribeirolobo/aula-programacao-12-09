e3f53df23e58533b1b12dec238ee9ffbe16b50ed
Igor Ribeiro Lobo <igorribeirolobo@gmail.com>
2017-09-12 19:41:41 -0300

    Java
    funcoes java

---
+++ src/Aula/Main.java
@@ -0,0 +1,12 @@
+package Aula;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		funcoes funcao = new funcoes();
+		
+
+	}
+
+}
---
+++ src/Aula/funcoes.java
@@ -0,0 +1,7 @@
+package Aula;
+
+public class funcoes {
+	
+	
+
+}
