10efc7cac36ef22f85fca6a8091eab1c67f78cab
Igor Ribeiro Lobo <igorribeirolobo@gmail.com>
2017-09-14 09:30:07 -0300

    Ex 12-09

--- src/Aula/Main.java
+++ src/Aula/Main.java
@@ -1,12 +1,20 @@
 package Aula;
 
+
+import javax.swing.JOptionPane;
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		funcoes funcao = new funcoes();
-		
-
+		funcao.qtdNum(funcao.lerNumero("Informe o numero: "));
+		for(int i = 0; i <= funcao.getQtd(); i++)
+		{
+			funcao.Somar(funcao.lerNumero("Informe o numero "+i+" : "));
+		}
+		JOptionPane.showMessageDialog(null,"A soma dos números é: "+funcao.getResultado());
 	}
+	
 
 }
--- src/Aula/funcoes.java
+++ src/Aula/funcoes.java
@@ -1,7 +1,56 @@
 package Aula;
 
-public class funcoes {
-	
-	
+import javax.swing.JOptionPane;
 
+public class funcoes {
+   private int num = 0;
+   private int resultado = 0;
+	public void Somar(int a)
+	{  
+		this.resultado = this.resultado + a;
+
+	}
+	public void qtdNum(int num)
+	{  boolean valida = true;
+	   while(valida)
+	   {
+		if(num < 2)
+		{
+			JOptionPane.showMessageDialog(null, "Informe mais de 2 numeros!");
+		}
+		else
+		{
+		this.num = num;
+		valida = false;
+     	}
+		
+	   }
+	}
+	public int getResultado()
+	{
+		return this.resultado;
+	}
+	public int getQtd()
+	{
+		return this.num;
+	}
+	public static int lerNumero(String mensagem)
+	{  int numero = 0;
+	   boolean valida = true;
+	   while(valida)
+	   {
+		   try {
+			   numero = Integer.parseInt(JOptionPane.showInputDialog(mensagem));
+			   valida = false;
+		   }
+		   catch(NumberFormatException e)
+		   {
+			   JOptionPane.showMessageDialog(null, "informe um numero válido, erro: "+ e.toString());
+	       }
+	   }
+	    
+
+		return numero;
+	}
+	
 }
